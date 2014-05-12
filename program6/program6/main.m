4522755551aa23212dd41a72d5bfad97a5087550
sandeep-chowdary <sandeep.pavulurii@gmail.com>
2014-05-12 13:53:30 -0400

    prog6

---
+++ program6/program6/main.m
@@ -0,0 +1,33 @@
+//
+//  main.m
+//  program6
+//
+//  Created by User1 on 2014-05-12.
+//  Copyright (c) 2014 ios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        int year , rem_4 , rem_100 , rem_400;
+        
+        NSLog (@"Enter the year to be tested:");
+        scanf ("%i", &year);
+        
+        rem_4 = year % 4;
+        rem_100 = year % 100;
+        rem_400 = year % 400;
+        
+        
+        if ( (rem_4 == 0 && rem_100 != 0)|| rem_400 == 0)
+            NSLog (@"It's a leap year.");
+        
+        else
+            NSLog (@"Nope,it's not a leap year.");
+        }
+    return 0;
+}
+
